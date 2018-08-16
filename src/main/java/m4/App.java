ea3bc926050855ee7815bca9c67b5bf17cd470c5
gyb123456 <578498005@qq.com>
2018-08-16 09:41:00 +0800

    test

--- src/main/java/m4/App.java
+++ src/main/java/m4/App.java
@@ -11,13 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        // 连接本地的 Redis 服务
-        Jedis jedis =  new Jedis("m4.beacool.com", 6379);
-        jedis.auth("I0tTest2o17");
-        jedis.select(1);
-        jedis.set("name","GYB-Test-Redis");
-        String name = jedis.get("name");
-        System.out.println("name = " + name);
-        jedis.close();
+
     }
 }
--- src/test/java/m4/AppTest.java
+++ src/test/java/m4/AppTest.java
@@ -3,6 +3,7 @@ package m4;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import redis.clients.jedis.Jedis;
 
 /**
  * Unit test for simple App.
@@ -16,5 +17,17 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    @Test
+    public void demo1( )
+    {
+        // 连接远程的 Redis 服务
+        Jedis jedis =  new Jedis("m4.beacool.com", 6379);
+        jedis.auth("I0tTest2o17");
+        jedis.select(1);
+        jedis.set("name","GYB-Test-Redis-1");
+        String name = jedis.get("name");
+        System.out.println("name = " + name);
+        jedis.close();
     }
 }
